1fd039aea7a9f3f43a637a7f23977e5ba9b20a05
imane-05er <erraqiouiimane17@gmail.com>
2025-05-17 15:50:54 +0100

    jhbhjubj<

---
+++ test.js
@@ -0,0 +1,13 @@
+et add=document.getElementById("add");
+let task=document.getElementById("task");
+add.addEventListener("click",(e)=>{
+    e.preventDefault();
+let input=document.createElement("input");
+input.setAttribute("type","checkbox")
+li.textContent=task.value;
+
+
+
+
+
+})
