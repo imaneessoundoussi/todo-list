66b1c4cc62654c304e91d5308143bd02487c8470
imane ess <imaneessoundoussi12@gmail.com>
2025-05-18 17:12:54 +0100

    hhfghg

--- test.js
+++ test.js
@@ -1,14 +1,25 @@
 const search = document.getElementById('search');
 const todoList = document.getElementById('todo-list');
-const todos = todoList.getElementsByTagName('li');
+const todos = document.querySelectorAll('todo-item');
 
-function filterTodos(term) {
-    for (let todo of todos) {
-        todo.style.display = todo.textContent.toLowerCase().includes(term) ? '' : 'none';
-    }
+function filterTodos(){
+    const search = document.getElementById('search').value.toLowerCase();
+    const todos = document.querySelectorAll('.todo-item');
+
+    todos.forEach(todo => {
+        const text = todo.textContent.toLocaleLowerCase();
+        if (text.includes(search)){
+            todo.style.display='flex';
+        }else{
+            todo.style.display = 'none';
+        }
+    })
+
+
+
 }
 search.addEventListener("keyup", () => {
         const term = search.value.trim().toLowerCase();
         filterTodos(term);
     });
-    
+
