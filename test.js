002c0493d07815bb98684c17e7daac65cc1d69e9
imane-05er <erraqiouiimane17@gmail.com>
2025-05-23 17:39:41 +0100

    jhjb

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 
-let input=document.getElementById("task");
-let add=document.getElementById("add");
+const input=document.getElementById("task");
+const add=document.getElementById("add");
+const ul=document.getElementById("todo-list");
+const list_box=document.getElementById("list_box");
+add.addEventListener("click",()=>{
+const li=document.createElement("li");
+li.innerHTML=input.value;
+list_box.appendChild(li);
+
+})
